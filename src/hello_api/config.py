f0990c15e79fbe5cc988259f926bd4a0688a29cf
shri-itsMe <shri.nallanc@gmail.com>
2025-07-06 22:06:51 -0400

    Dynamic access to the parent folder

--- src/hello_api/config.py
+++ src/hello_api/config.py
@@ -1,9 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+import os
+from pathlib import Path
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-    proj_name: str = "Hello FastAPI Project"
+    # proj_name: str = "Hello FastAPI Project"
+    os.environ["PROJ_NAME"] = Path(__file__).parent.name # Retrieves the parent directory of this python package folder
+
+    proj_name: str = os.getenv("PROJ_NAME") # Puts in the env var
 
 class Config:
     env_file = '.env'
